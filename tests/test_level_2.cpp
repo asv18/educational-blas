f8a3aaa037c366dffe65a0364b4a5bd30864607e
Aarnav Srivastava <70161949+AarnavSrivastava@users.noreply.github.com>
2026-05-19 23:43:52 -0400

    adding test_level_2.cpp

---
+++ tests/test_level_2.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <cfloat>
+
+#include "blas.hpp"
+
+using namespace testing;
+using namespace mblas;
